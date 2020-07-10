bb5aeacf146d816a27bbd9f1055021100f10fec3
Lala <luangu2020@gmail.com>
2020-07-09 20:29:35 -0400

    mailtrap.io

--- app.js
+++ app.js
@@ -34,45 +34,45 @@ app.post('/send', (req, res) => {
    <h3>Message</h3>
    <p>${req.body.message}</p>
   `;
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.ethereal.email',
+  //   port: 587,
+  //   auth: {
+  //     user: 'josianne.ullrich@ethereal.email',
+  //     pass: 'QC9ZgFHy8AKr1TYHqG'
+  //   }
+  // });
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ab4d5badf5923e",
+      pass: "c94ab9f6b80d78"
+    }
+  });
+
+  // send mail with defined transport object
+  let mailOptions = {
+    from: '"Recipe Box" < RecipeBox@recipebox.com>', // sender address
+    //to: "luangu2020@gmail.com",
+    to: `${req.body.email}`, // list of receivers
+    subject: "Your Recipe", // Subject line
+    text: "Here you go", // plain text body
+    html: output // html body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render('contact', { msg: "Email has been sent" });
+  });
 });
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
-  auth: {
-    user: 'josianne.ullrich@ethereal.email',
-    pass: 'QC9ZgFHy8AKr1TYHqG'
-  }
-});
-
-// const transport = nodemailer.createTransport({
-//   host: "smtp.mailtrap.io",
-//   port: 2525,
-//   auth: {
-//     user: "ab4d5badf5923e",
-//     pass: "c94ab9f6b80d78"
-//   }
-// });
-
-// send mail with defined transport object
-let mailOptions = {
-  from: '"Nodemailer contact" < josianne.ullrich@ethereal.email>', // sender address
-  //to: "luangu2020@gmail.com",
-  to: `${req.body.email}`, // list of receivers
-  subject: "Your Recipe", // Subject line
-  text: "Here you go", // plain text body
-  //html: output // html body
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    return console.log(error);
-  }
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-  res.render('contact', { msg: "Email has been sent" });
-});
 
 
 
